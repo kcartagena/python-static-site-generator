67b1f2237b4f33ab626a61cc05b6210525d2d309
Katie Cartagena <katiecartagena@hotmail.com>
2020-06-16 13:49:25 -0500

    Module2 completed through step 6

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -4,4 +4,19 @@ from pathlib import Path
 class Parser:
     extensions: List[str] = []
 
-    
+    def valid_extension(self, extension):
+        return extension in self.extensions
+
+    def parse(self: Path, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
+
+    def read(self, path):
+        with open(path, "r") as file:
+            return file.read()
+
+    def write(self, path, dest, content, ext=".html"):
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path, "w") as file:
+            file.write(content)
+        
+
