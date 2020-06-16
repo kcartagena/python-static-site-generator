1fd7131146b9354788926cec707b73aa5f92a94e
Katie Cartagena <katiecartagena@hotmail.com>
2020-06-16 14:26:33 -0500

    Module2 through step 10

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,3 +1,5 @@
+import shutil
+
 from typing import List
 from pathlib import Path
 
@@ -18,5 +20,15 @@ class Parser:
         full_path = dest / path.with_suffix(ext).name
         with open(full_path, "w") as file:
             file.write(content)
+
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
+    
+
+class ResourceParser(Parser):
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+    
+    def parse(self, path, source, dest):
+        self.copy(path, source, dest)
         
 
