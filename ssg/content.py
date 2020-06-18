34cd58de963630c86d692d782a36bc3b415283c8
Katie Cartagena <katiecartagena@hotmail.com>
2020-06-18 16:31:52 -0500

    Module 3 Type Complete

--- ssg/content.py
+++ ssg/content.py
@@ -5,7 +5,7 @@ from collections.abc import Mapping
 
 
 class Content(Mapping):
-    __delimiter = r"^(?:-|+){3}\s*$"
+    __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
 
     @classmethod
@@ -25,6 +25,30 @@ class Content(Mapping):
     @property
     def type(self):
         return self.data["type"] if "type" in self.data else None
+
+    @type.setter
+    def type(self, type):
+        self.data["type"] = type
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __iter__(self):
+        return self.data.__iter__()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        data = {}
+        for key, value in self.data.items():
+            if key != "content":
+                data[key] = value
+        return str(data)
+
+    
+
+    
 
     
 
